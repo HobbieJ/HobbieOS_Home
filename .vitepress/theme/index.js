29338adcbd7148383b16b181205a6b3084187c49
Jacob Hobbie <hey@jacobhobbie.com>
2024-05-29 03:55:07 +0000

    setting up tailwindcss

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import Layout from './Layout.vue'
-import './style.css'
+import './style.postcss'
 
 /** @type {import('vitepress').Theme} */
 export default {
