0333bb9fe1394cd33a917f96aa1d0a0ba8dfbf8b
Jacob Hobbie <hey@jacobhobbie.com>
2024-06-02 04:52:00 +0000

    implementing data attribute for pulling post data

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import PostCard from './PostCard.vue'
+import { data as posts } from './../posts.data'
 </script>
 
 <template>
@@ -7,9 +8,7 @@ import PostCard from './PostCard.vue'
         <div class="w-full max-w-screen-xl m-auto">
             <!-- TODO: Implement Programmable Icon Changer -->
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                <PostCard />
-                <PostCard />
-                <PostCard />
+                <PostCard v-for="post of posts" :title="post.frontmatter.title"/>
             </div>
         </div>
     </main>
