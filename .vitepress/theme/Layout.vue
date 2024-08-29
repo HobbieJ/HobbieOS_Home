97149c5112f357d687738a7e2b28e5aab72c8b02
Jacob Hobbie <hey@jacobhobbie.com>
2024-08-29 00:20:26 -0500

    scaffolding final sitemap

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -3,6 +3,10 @@ import { useData } from 'vitepress'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import Home from './pages/Home.vue'
+import Now from './pages/Now.vue'
+import About from './pages/About.vue'
+import Contact from './pages/Contact.vue'
+import Dating from './pages/Dating.vue'
 import NotFound from './pages/NotFound.vue'
 
 // https://vitepress.dev/reference/runtime-api#usedata
@@ -15,7 +19,12 @@ const { site, frontmatter } = useData()
       <Header :title="site.title" />
       <main class="pt-24 max-w-screen-xl mx-auto">
         <Home v-if="frontmatter.home" />
-        <!-- <Content v-else /> -->
+        <Now v-else-if="frontmatter.now" />
+        <About v-else-if="frontmatter.about" />
+        <Contact v-else-if="frontmatter.contact" />
+        <Dating v-else-if="frontmatter.dating" />
+        <NotFound v-else-if="page.isNotFound" />
+        <Content v-else />
       </main>
       <!-- <Footer /> -->
     </div>
